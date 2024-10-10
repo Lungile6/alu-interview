d4783576ce051322ae11f2f6b0177b6ba3d1d9f1
Lungile6 <l.mabelebel@student.com>
2024-10-10 18:50:22 +0200

    Completing the Pascal triangle

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Pascal triangle
+"""
+
+
+def pascal_triangle(x):
+    # Check if the input is less than or equal to 0
+    if x <= 0:
+        return []  # Return an empty list for non-positive input
+
+    triangle = []  # Initialize the list to hold the rows of the triangle
+
+    # Loop through the number of rows specified by x
+    for row in range(x):
+        new_row = [1] * (row + 1)  # Start each row with 1s
+        # Calculate the values for the current row based on the previous row
+        for y in range(1, row):
+            new_row[y] = triangle[row - 1][y - 1] + triangle[row - 1][y]
+        triangle.append(new_row)  # Add the completed row to the triangle
+    return triangle  # Return the completed Pascal's triangle
